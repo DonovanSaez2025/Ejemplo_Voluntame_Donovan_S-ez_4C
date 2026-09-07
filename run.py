cc15e57338de529a93e79db73a59c90524e603f7
DonovanSaez2025 <donovansaez@liceovvh.cl>
2026-09-07 10:42:37 -0300

    x

--- run.py
+++ run.py
@@ -8,5 +8,3 @@ from flask_app.controllers.controlador_misiones import misiones_bp
 app.register_blueprint(usuarios_bp)
 app.register_blueprint(misiones_bp)
 
-if __name__ == "__main__":
-    app.run(debug=True)
